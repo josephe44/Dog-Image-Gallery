efb24f5a74f038f4e8865ddc83952b68bfcbd40e
Osuohia Emmanuel <josephe442@gmail.com>
2021-07-14 01:25:02 +0100

    remove unused code

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const gallery = document.getElementById('gallery')
 const selectedImage = document.getElementById('selectedImage')
 const popup = document.getElementById('popup')
-// const imageIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // outputting all images using the forEach array method and creating an img element, then append it to the gallery which is the 'div' parent.
 for (let i = 1; i <= 10; i++) {
